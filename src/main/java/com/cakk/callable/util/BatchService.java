461330c954672dd9d5e8abc8de7414cbd054fa90
pwang313@gmail.com <pwang313@gmail.com>
2020-05-11 16:52:14 -0400

    add synchronized to let all functionality work

--- src/main/java/com/cakk/callable/util/BatchService.java
+++ src/main/java/com/cakk/callable/util/BatchService.java
@@ -16,26 +16,45 @@ import com.cakk.callable.service.ProcessLimit;
 
 public final class BatchService {
 	public static void process() throws InterruptedException, ExecutionException {
-		int limitNumber = 0;
+		int limitIndex = 0;
+		Object processingLock = new Object();
+		Object resultLock = new Object();
 		List<Limit> totalList = generateProcessList();
 		List<Limit> processedList = new ArrayList<>();
 
 		List<Limit> processingList = new ArrayList<>();
 		ExecutorService service = Executors.newFixedThreadPool(ServiceConstants.THREADS);
 		//submit 10 to pool first
-		List<Future<Integer>> results = new ArrayList<>();
+		List<Future<Integer>> resultList = new ArrayList<>();
 		for(int i = 0; i < ServiceConstants.THREADS; i++) {	//this result will block here
-			limitNumber++;
+			limitIndex++;
 			System.out.println("Submit:"+i);
-			results.add(service.submit(new ProcessLimit(i)));
+			resultList.add(service.submit(new ProcessLimit(i)));
 			processingList.add(new Limit(i, false));		//list in thread pool
 			
 		}
-		while(limitNumber < ServiceConstants.LIMIT_NUMBER || processingList.size() > 0) {
-			List<Future<Integer>> doneList = results.stream()
-				.filter(r->r.isDone())
-				.collect(Collectors.toList());
+		while(limitIndex < ServiceConstants.LIMIT_NUMBER || processingList.size() > 0) {
+			List<Future<Integer>> doneList = null;
+			synchronized(resultLock) {
+				doneList = resultList.stream()
+					.filter(r->r.isDone())
+					.collect(Collectors.toList());
+			}
+
 				
+			if (ServiceConstants.COMMAND == ServiceConstants.COMMAND_RESUME &&
+					limitIndex < ServiceConstants.LIMIT_NUMBER) {
+				
+				System.out.println("limitIndex:"+limitIndex);
+				synchronized(processingLock) {
+					for(int i = 0; i < ServiceConstants.LIMIT_NUMBER - processingList.size(); i++) {
+						processingList.add(totalList.get(limitIndex));		//list in thread pool
+					}
+				}
+				synchronized(resultLock) {
+					resultList.add(service.submit(new ProcessLimit(totalList.get(limitIndex++).getItem())));
+				}
+			}
 			for(Future<Integer> r: doneList) {
 				AtomicInteger ai = new AtomicInteger(0);
 				try {
@@ -51,9 +70,11 @@ public final class BatchService {
 					//printLimitList(processedList);
 					//System.out.println("Before. processing:");
 					//printLimitList(processingList);
-					processingList = processingList.stream()
-							.filter(t->ai.get() != t.getItem())
-							.collect(Collectors.toList());
+					synchronized(processingLock) {
+						processingList = processingList.stream()
+								.filter(t->ai.get() != t.getItem())
+								.collect(Collectors.toList());
+					}
 					//System.out.println("After. processing:");
 					//printLimitList(processingList);
 				} catch (InterruptedException e) {
@@ -62,16 +83,23 @@ public final class BatchService {
 					e.printStackTrace();
 				}
 				//System.out.println("processingList:"+processingList.size());
-				results.remove(r);
+				synchronized(resultLock) {
+					resultList.remove(r);
+				}
 				if(ServiceConstants.COMMAND == ServiceConstants.COMMAND_STOP && processingList.size() == 0) {
 					System.out.println("Received STOP command, all items on the list processed.");
 					break;
 				}
-				if(ServiceConstants.COMMAND == ServiceConstants.COMMAND_START || 
-					ServiceConstants.COMMAND == ServiceConstants.COMMAND_RESUME ) {
-					if(limitNumber < ServiceConstants.LIMIT_NUMBER) {
-						processingList.add(totalList.get(limitNumber));
-						results.add(service.submit(new ProcessLimit(totalList.get(limitNumber++).getItem())));
+				if(ServiceConstants.COMMAND == ServiceConstants.COMMAND_START  || 
+						ServiceConstants.COMMAND == ServiceConstants.COMMAND_RESUME) {
+					if(limitIndex < ServiceConstants.LIMIT_NUMBER) {
+						System.out.println("limitIndex:"+limitIndex);
+						synchronized(processingLock) {
+							processingList.add(totalList.get(limitIndex));
+						}
+						synchronized(resultLock) {
+							resultList.add(service.submit(new ProcessLimit(totalList.get(limitIndex++).getItem())));
+						}
 					}
 				}
 			}
